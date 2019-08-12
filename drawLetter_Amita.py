4c0527215cf711a5b52e48876b65a2d9ba5fd3b4
Amita Stowitts <spis19bh@its-cseb230-21.ucsd.edu>
2019-08-12 15:36:29 -0700

    Made turtle go :)

--- drawLetter_Amita.py
+++ drawLetter_Amita.py
@@ -1,6 +1,8 @@
 import turtle
 
+# draws the letter a with the turtle
 def drawA(theTurtle):
+    """ this function takes a turtle and draws the letter A"""
     theTurtle.left(75)
     theTurtle.forward(300)
     theTurtle.right(150)
@@ -10,6 +12,7 @@ def drawA(theTurtle):
     theTurtle.left(75)
     theTurtle.forward(80)
 
+# makes a turtle object and calls the drawA function
 aTurtle = turtle.Turtle()
 aTurtle.speed(1)
 drawA(aTurtle)
